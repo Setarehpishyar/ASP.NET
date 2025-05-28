d418d8caab993bdc833909ea3e120988c70d5aa3
Setareh pishyar <140304083+Setarehpishyar@users.noreply.github.com>
2025-05-28 23:09:16 +0100

    Add files via upload

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+    }
+}
